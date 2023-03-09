42c87b2bedd7279c057f49e8467b68c0caf0cc8c
Eduardo <edu.he.dev@gmail.com>
2023-03-09 01:49:16 -0300

    tipos de variaveis em php

--- Bases/tipoDeVariavel.php
+++ Bases/tipoDeVariavel.php
@@ -3,9 +3,11 @@
 $string = "Ola mundo";
 $integer = 10;
 $float = 10.5;
-
+$boolean = true;
 
 echo $string . " é " . gettype($string) . "\n";
 echo $integer . " é " . gettype($integer) . "\n";
 echo $float . " é " . gettype($float) . "\n";
+echo $boolean . " é " . gettype($boolean) . "\n";
+
 
