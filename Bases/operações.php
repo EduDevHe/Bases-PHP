afa47a19bffba86eb3dcfba60ab4eb5d77386cab
Eduardo <edu.he.dev@gmail.com>
2023-03-09 01:38:53 -0300

    operações em php

---
+++ Bases/operações.php
@@ -0,0 +1,16 @@
+<?php
+
+$idade = 21;
+
+$idadeMais10 = $idade + 10;
+$idadeMenos10 = $idade - 10;
+$idadeVezes10 = $idade * 10;
+$idadeDividida10 = $idade / 10;
+
+echo $idade . "\n";
+echo $idadeMais10 . "\n";
+echo $idadeMenos10 . "\n";
+echo $idadeVezes10 . "\n";
+echo $idadeDividida10 . "\n";
+
+
